f54688efe2bb7fb81d2d7041d72501b73fcec028
Renandatta Rooziq <renandattarooziq@gmail.com>
2020-07-26 23:01:02 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Coupon;
+use App\Period;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __construct()
+    protected $period, $coupon;
+    public function __construct(Period $period, Coupon $coupon)
     {
         $this->middleware('auth');
+        $this->period = $period;
+        $this->coupon = $coupon;
     }
 
     public function index()
     {
-        return view('home.index');
+        $periods = $this->period->orderBy('id', 'desc')->get();
+        foreach ($periods as $key => $period) {
+            $periods[$key]->claimed = $this->coupon->where('period_id', '=', $period->id)
+                ->where('is_claim', '=', 1)->count();
+            $periods[$key]->not_claimed = $this->coupon->where('period_id', '=', $period->id)
+                ->where('is_claim', '=', 0)->count();
+        }
+        return view('home.index', compact('periods'));
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,8 +5,27 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid pt-4 pb-5">
-
+    <div class="container pt-4 pb-5">
+        @foreach($periods as $period)
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-md-6 border-right">
+                            <h5 class="mb-0">Periode : </h5>
+                            <h3 class="mb-0">{{ $period->name }}</h3>
+                        </div>
+                        <div class="col-md-3 border-right text-right">
+                            <h5 class="mb-0">Sudah Diambil : </h5>
+                            <h3 class="mb-0">{{ $period->claimed }}</h3>
+                        </div>
+                        <div class="col-md-3 text-right">
+                            <h5 class="mb-0">Belum Diambil : </h5>
+                            <h3 class="mb-0">{{ $period->not_claimed }}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 @endsection
 
