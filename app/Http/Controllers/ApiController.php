6e8ede71fcc06c92a0e0e13b892ee98bd889fcb7
Renandatta Rooziq <renandattarooziq@gmail.com>
2020-07-29 16:56:27 +0700

    api android

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Coupon;
+use App\Period;
+use App\User;
+use App\UserAuth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ApiController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|min:4|max:255',
+            'password' => 'required|min:3|:max:255'
+        ]);
+
+        $user = User::where('email', '=', $request->input('email'))->first();
+        if (empty($user)) return response()->json(['error' => 'Email salah !']);
+        if (Hash::check($request->input('password'), $user->password)) {
+            $token = Str::random(16);
+            UserAuth::create([
+                'user_id' => $user->id,
+                'auth' => 'login',
+                'token' => $token
+            ]);
+            $user->token = $token;
+            return response()->json(['success' => $user]);
+        }
+        return response()->json(['error' => 'Password Salah !']);
+    }
+
+    public function logout(Request $request)
+    {
+        $userAuth = UserAuth::where('token', '=', $request->input('token'))->update(['auth' => 'logout']);
+        return response()->json(['success' => $userAuth]);
+    }
+
+    public function period_search(Request $request)
+    {
+        if (!$request->has('token')) return response()->json(['error' => 'Parameter Tidak Sesuai !']);
+        $userAuth = UserAuth::where('token', '=', $request->input('token'))
+            ->where('auth', '=', 'login')
+            ->first();
+        if (empty($userAuth)) return response()->json(['error' => 'Token Kadaluarsa !']);
+        $periods = Period::orderBy('id', 'desc')->get();
+        return response()->json(['success' => $periods]);
+    }
+
+    public function claim(Request $request)
+    {
+        if (!$request->has('token')) return response()->json(['error' => 'Parameter Tidak Sesuai !']);
+        $userAuth = UserAuth::where('token', '=', $request->input('token'))
+            ->where('auth', '=', 'login')
+            ->first();
+        if (empty($userAuth)) return response()->json(['error' => 'Token Kadaluarsa !']);
+        if (!$request->has('period_id') || !$request->has('no_coupon'))
+            return response()->json(['error' => 'Parameter Tidak Sesuai !']);
+        $coupon = Coupon::where('period_id', '=', $request->input('period_id'))
+            ->where('no_coupon', '=', $request->input('no_coupon'))
+            ->update([
+                'is_claim' => 1,
+                'claim_at' => date('Y-m-d H:is'),
+                'claim_media' => 'Android'
+            ]);
+        return response()->json(['success' => $coupon]);
+    }
+}
